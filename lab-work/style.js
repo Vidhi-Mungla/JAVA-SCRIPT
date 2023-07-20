e872979fe238f0fef46cf3e905d1a70af268a71f
Vidhi-Mungla <134635193+Vidhi-Mungla@users.noreply.github.com>
2023-07-20 17:04:21 +0530

    .

---
+++ lab-work/style.js
@@ -0,0 +1,15 @@
+//document.write("this is external JS...!");
+/*
+
+document.getElementById('para').innerHTML = "This is paragraph..!"
+console.log("console...!");
+
+*/
+
+
+window.alert("alert...!");
+window.confirm("Are you sure!");
+prompt("5+5 = ?");
+
+
+window.print();
